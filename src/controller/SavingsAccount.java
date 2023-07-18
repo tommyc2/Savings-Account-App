628b8b9d8c000670a6a6ef8ca0ee5c2f8f311124
Tommy Condon <tcondon12@icloud.com>
2023-07-18 21:34:24 +0100

    Progress bar

--- src/controller/SavingsAccount.java
+++ src/controller/SavingsAccount.java
@@ -16,6 +16,11 @@ public class SavingsAccount {
 
     private List<Lodgement> lodgements;
 
+    // Fields //
+    private int target = 3250;
+    private final String greenText = "\u001B[32m";
+    private final String resetColour = "\u001B[0m";
+
     public SavingsAccount() {
         lodgements = new ArrayList<>();
     }
@@ -64,6 +69,34 @@ public class SavingsAccount {
         return balance / 3250;
     }
 
+    public String calculateProgressBar(double progressVariable){
+
+        String progressFormat = "[----------------]";
+
+        if ((progressVariable >= 5) && (progressVariable <= 20)){
+            progressFormat = greenText + "███" + resetColour + "-------------]";
+        }
+
+        if ((progressVariable > 20) && (progressVariable <= 40)){
+            progressFormat = greenText + "██████" + resetColour + "----------]";
+        }
+
+        if ((progressVariable > 40) && (progressVariable <= 60)){
+            progressFormat = greenText +"█████████" + resetColour + "-------]";
+        }
+
+        if ((progressVariable > 60) && (progressVariable <= 90)){
+            progressFormat = greenText +"█████████████" + resetColour + "---]";
+        }
+
+        if ((progressVariable > 90) && (progressVariable <= 100.0)){
+            progressFormat = greenText +"█████████████████" + resetColour;
+        }
+
+        return progressFormat;
+    }
+
+
     public String displayInsights() {
         String insights = "";
 
@@ -73,12 +106,15 @@ public class SavingsAccount {
 
         // Rounding percentage completed variable to two decimal places
         DecimalFormat decimalFormat = new DecimalFormat("#.##");
-        double roundedPercentageCompleted = Double.parseDouble(decimalFormat.format(percentCompleted));
+        String stringFormat = decimalFormat.format(percentCompleted);
+        double roundedPercentageCompleted = Double.parseDouble(stringFormat);
 
         insights += "----- Insights -------\n";
-        insights += "| Balance: " + balance + "\n";
+        insights += "| Balance: " + "€" + balance + "\n";
         insights += "| % of Goal achieved: " + roundedPercentageCompleted + " %" + "\n";
+        insights += "" + calculateProgressBar(roundedPercentageCompleted) + "\n";
         insights += "| No. of Lodgements made: " + numberOfLodgements + "\n";
+        insights += "| Target: " + "€" + this.target + "\n" ;
         insights += "-----------------------\n";
 
         return insights;
